825e8d61c223b469baa0cbe5162b95ed58d88f8e
Ramsey Ith Njema <rnjema@unima.ac.mw>
2025-04-02 11:11:30 +0200

    Update. Exit on error

--- lab_03/tcp_server_with_executor.py
+++ lab_03/tcp_server_with_executor.py
@@ -14,6 +14,7 @@ import logging
 import threading
 from concurrent.futures import ThreadPoolExecutor
 import argparse
+import sys
 
 def parse_args():
     parser = argparse.ArgumentParser(description="A simple multithreaded TCP echo server using executor.")
